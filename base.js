6e6a1830e3ee25e0792d5785130d4836e69a59e9
Coarse Rosinflower <113859223+cD1rtX3@users.noreply.github.com>
2024-03-18 18:11:23 -0400

    ???

--- base.js
+++ base.js
@@ -1,12 +1,1 @@
-let cur_string = "";
-
-// Listening for keypresses.
-document.addEventListener("keydown", function (e) {
-	if (e.key = "Backspace" && cur_string) {
-		cur_string = cur_string.substring(-1);
-	} else {
-		cur_string += e.key;
-	}
-	cursor_x = 8;
-	text("italic 36px", cur_string);
-});
+text("italic 36px", "foo", 0, 0);
