f43a0d801ed94e0bfdea55bde26d71a391a92bf7
Coarse Rosinflower <113859223+cD1rtX3@users.noreply.github.com>
2024-03-18 18:00:26 -0400

    Update dummy script

--- base.js
+++ base.js
@@ -1,2 +1,13 @@
-// Hello, world!
-ctx.fillText("Hello, world!", 8, 40);
+let cur_string = "";
+
+// Listening for keypresses.
+document.addEventListener("keydown", function (e) {
+	if (e.key = "Backspace" && cur_string) {
+		cur_string = cur_string.substring(-1);
+	} else {
+		cur_string += e.key;
+	}
+	ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+	cursor_x = 8;
+	text("italic 36px", cur_string);
+});
